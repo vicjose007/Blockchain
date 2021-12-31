32d5e31376e913cc1a0a0a6b8d63a87a7e3067bd
anagparedes <anagparedesg@gmail.com>
2021-12-31 09:05:08 -0400

    add methods to main

--- BlockChain.cs
+++ BlockChain.cs
@@ -134,6 +134,19 @@ namespace Blockchain
 
         static void Main(string[] args)
         {
+             Random rnd = new Random(DateTime.UtcNow.Millisecond);
+            IBlock genesis = new Block(new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00 });
+            byte[] difficulty = new byte[] { 0x00, 0x00 };
+
+            BlockChain chain = new BlockChain(difficulty, genesis);
+            for (int i = 0; i < 200; i++)
+            {
+                var data = Enumerable.Range(0, 2256).Select(p => (byte)rnd.Next());
+                chain.Add(new Block(data.ToArray()));
+                Console.WriteLine(chain.LastOrDefault()?.ToString());
+            }
+
+            Console.ReadLine();
 
         }
     }
