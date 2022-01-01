11fecc580c0a89e47fbc677eecf82f011f792068
anagparedes <anagparedesg@gmail.com>
2022-01-01 13:23:58 -0400

    Organize classes

--- BlockChain.cs
+++ BlockChain.cs
@@ -7,55 +7,7 @@ using System.Security.Cryptography;
 
 namespace Blockchain
 {
-    public static class BlockChainExtension
-    {
-        public static byte[] GenerateHash(this IBlock block)
-        {
-            using (SHA512 sha = new SHA512Managed())
-            using (MemoryStream st = new MemoryStream())
-            using (BinaryWriter bw = new BinaryWriter(st))
-            {
-                bw.Write(block.Data);
-                bw.Write(block.Nonce);
-                bw.Write(block.TimeStamp.ToBinary());
-                bw.Write(block.prevHash);
-                var starr = st.ToArray();
-                return sha.ComputeHash(starr);
-            }
-        }
-        public static byte[] MineHash(this IBlock block, byte[] difficulty)
-        {
-            if (difficulty == null) throw new ArgumentNullException(nameof(difficulty));
-            byte[] hash = new byte[0];
-            int d = difficulty.Length;
-            while (!hash.Take(2).SequenceEqual(difficulty))
-            {
-                block.Nonce++;
-                hash = block.GenerateHash();
-            }
-            return hash;
-        }
-        public static bool IsValid(this IBlock block)
-        {
-            var bk = block.GenerateHash();
-            return block.Hash.SequenceEqual(bk);
-        }
-        public static bool IsValidPrevBlock(this IBlock block, IBlock prevBlock)
-        {
-            if (prevBlock == null) throw new ArgumentNullException(nameof(prevBlock));
-            var prev = prevBlock.GenerateHash();
-            return prevBlock.IsValid() && block.prevHash.SequenceEqual(prev);
-
-        }
-        public static bool IsValid(this IEnumerable<IBlock> items)
-        {
-            var enumerable = items.ToList();
-            return enumerable.Zip(enumerable.Skip(1), Tuple.Create).All(block => block.Item2.IsValid() && block.Item2.IsValidPrevBlock(block.Item1));
-        }
-
-
-    }
-    public interface IBlock
+     public interface IBlock
     {
         byte[] Data { get; }
         byte[] Hash { get; set; }
@@ -89,7 +41,7 @@ namespace Blockchain
         }
 
     }
-
+    
     public class BlockChain : IEnumerable<IBlock>
     {
         private List<IBlock> _items = new List<IBlock>();
@@ -139,6 +91,55 @@ namespace Blockchain
         }
     }
 
+    public static class BlockChainExtension
+    {
+        public static byte[] GenerateHash(this IBlock block)
+        {
+            using (SHA512 sha = new SHA512Managed())
+            using (MemoryStream st = new MemoryStream())
+            using (BinaryWriter bw = new BinaryWriter(st))
+            {
+                bw.Write(block.Data);
+                bw.Write(block.Nonce);
+                bw.Write(block.TimeStamp.ToBinary());
+                bw.Write(block.prevHash);
+                var starr = st.ToArray();
+                return sha.ComputeHash(starr);
+            }
+        }
+        public static byte[] MineHash(this IBlock block, byte[] difficulty)
+        {
+            if (difficulty == null) throw new ArgumentNullException(nameof(difficulty));
+            byte[] hash = new byte[0];
+            int d = difficulty.Length;
+            while (!hash.Take(2).SequenceEqual(difficulty))
+            {
+                block.Nonce++;
+                hash = block.GenerateHash();
+            }
+            return hash;
+        }
+        public static bool IsValid(this IBlock block)
+        {
+            var bk = block.GenerateHash();
+            return block.Hash.SequenceEqual(bk);
+        }
+        public static bool IsValidPrevBlock(this IBlock block, IBlock prevBlock)
+        {
+            if (prevBlock == null) throw new ArgumentNullException(nameof(prevBlock));
+            var prev = prevBlock.GenerateHash();
+            return prevBlock.IsValid() && block.prevHash.SequenceEqual(prev);
+
+        }
+        public static bool IsValid(this IEnumerable<IBlock> items)
+        {
+            var enumerable = items.ToList();
+            return enumerable.Zip(enumerable.Skip(1), Tuple.Create).All(block => block.Item2.IsValid() && block.Item2.IsValidPrevBlock(block.Item1));
+        }
+
+
+    }
+   
     class Program
     {
 
